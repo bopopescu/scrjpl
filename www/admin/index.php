6ebd8dfebdca2541442d4b65c48b58d49ccf6640
detourbr <detourbr@github.com>
2014-10-17 00:24:48 +0200

    MAJ interface web

--- www/admin/index.php
+++ www/admin/index.php
@@ -11,22 +11,68 @@
     <meta name="author" content="Brian">
 
     <link rel="stylesheet" href="res/css/styles.css">
-    <link rel="stylesheet" href="res/css/font.css">
 </head>
 
 <body>
     <nav class="topbar">
-        <div class="topbar-title">DART Manager</div>
+        <div class="topbar-title">Bienvenue sur le DART Manager</div>
     </nav>
     <ul class="navbar">
         <li>
             <a href="index.php"><i class="navbar-icon navbar-icon-dashboard"></i>Dashboard</a>
         </li>
         <li>
+            <a href="index.php"><i class="navbar-icon navbar-icon-network"></i>Manager</a>
+        </li>
+        <li>
             <a href="docu.php"><i class="navbar-icon navbar-icon-doc"></i>Documentation</a>
         </li>
+        <li>
+            <a href="logout.php"><i class="navbar-icon navbar-icon-logout"></i>Déconnexion</a>
+        </li>
     </ul>
-    <div></div>
-    <script src="js/scripts.js"></script>
+    <div class="wrapper">
+        <div class="panel panel-blue">
+            <div class="panel-container">
+                <div class="panel-top">
+                    <i class="panel-top-icon"></i>
+                </div>
+                <a href="detail.php">
+                    <div class="panel-bottom">Détails<i class="panel-detail-icon"></i></div>
+                </a>
+            </div>
+        </div>
+        <div class="panel panel-red">
+            <div class="panel-container">
+                <div class="panel-top">
+                    <i class="panel-top-icon"></i>
+                </div>
+                <a href="detail.php">
+                    <div class="panel-bottom">Détails<i class="panel-detail-icon"></i></div>
+                </a>
+            </div>
+        </div>
+        <div class="panel panel-green">
+            <div class="panel-container">
+                <div class="panel-top">
+                    <i class="panel-top-icon"></i>
+                </div>
+                <a href="detail.php">
+                    <div class="panel-bottom">Détails<i class="panel-detail-icon"></i></div>
+                </a>
+            </div>
+        </div>
+        <div class="panel panel-yellow">
+            <div class="panel-container">
+                <div class="panel-top">
+                    <i class="panel-top-icon"></i>
+                </div>
+                <a href="detail.php">
+                    <div class="panel-bottom">Détails<i class="panel-detail-icon"></i></div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- <script src="js/scripts.js"></script> -->
 </body>
 </html>
