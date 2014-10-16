23cf692b8fa9388fc9165a22b2bd65a9b45dd490
detourbr <detourbr@github.com>
2014-10-16 16:26:18 +0200

    Commencement de l'interface d'administration

---
+++ www/admin/index.php
@@ -0,0 +1,32 @@
+<?php
+
+ ?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+
+    <title>Panneau de contrôle</title>
+    <meta name="description" content="Panneau de contrôle des JOG">
+    <meta name="author" content="Brian">
+
+    <link rel="stylesheet" href="res/css/styles.css">
+
+</head>
+
+<body>
+    <nav class="topbar">
+        <div class="topbar-title">Projet JOG 2.0</div>
+    </nav>
+    <ul class="navbar">
+        <li>
+            <a href="index.php"><i class="navbar-icon navbar-icon-dashboard"></i>Dashboard</a>
+        </li>
+        <li>
+            <a href="docu.php"><i class="navbar-icon navbar-icon-doc"></i>Documentation</a>
+        </li>
+    </ul>
+    <div></div>
+    <script src="js/scripts.js"></script>
+</body>
+</html>
